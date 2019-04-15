f9d4fa46163ecf9c04748a3369f2ffc8d112936d
Chris Troutner <chris.troutner@gmail.com>
2019-04-15 12:47:02 -0700

    Added npm start

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # memo-test
+A scratchpad for working with BITBOX, BCH, and memo.cash.
---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+  A scratchpad for working with BITBOX, BCH, and memo.cash.
+*/
+
+'use strict'
+
+console.log('Hello world')
