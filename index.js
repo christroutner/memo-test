42ef6320e2e90b3ac71dc94713e8feee33f60b05
Chris Troutner <chris.troutner@gmail.com>
2019-04-15 14:15:17 -0700

    Prototyped getting memo transactions

--- index.js
+++ index.js
@@ -2,6 +2,32 @@
   A scratchpad for working with BITBOX, BCH, and memo.cash.
 */
 
-'use strict'
+"use strict";
 
-console.log('Hello world')
+const BITBOXSDK = require("bitbox-sdk");
+const BITBOX = new BITBOXSDK();
+
+// Used for debugging and iterrogating JS objects.
+const util = require("util");
+util.inspect.defaultOptions = { depth: 1 };
+
+const ADDR = "bitcoincash:qq34qnz6527rp2szzkull8dzrkmmrnlfuq4ua74spq";
+
+async function runTest() {
+  const details = await BITBOX.Address.details(ADDR);
+  //console.log(`details: ${JSON.stringify(details,null,2)}`)
+
+  const TXIDs = details.transactions
+
+  for(let i=0; i < TXIDs.length; i++) {
+    const thisTXID = TXIDs[i]
+
+    //const thisTx = await BITBOX.RawTransactions.getRawTransaction(thisTXID, true)
+    //console.log(`thisTx: ${JSON.stringify(thisTx,null,2)}`)
+
+    const thisTx = await BITBOX.Transaction.details(thisTXID)
+    console.log(`thisTx: ${JSON.stringify(thisTx,null,2)}`)
+
+  }
+}
+runTest();
